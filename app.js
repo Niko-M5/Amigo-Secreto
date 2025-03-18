5f33716e35bb166cb6deb65196c0c9a6a9404221
Niko-M5 <alf.nicolasmartinez@gmail.com>
2025-03-18 19:12:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
-// Lista de los nombres de los amigos secretos
+
 let amigos = [];
 
-// Funcion para agregar nombres a la lista
 function agregarAmigo() {
-    const texto = document.getElementById("amigo"); // Obtiene el elemento con id de amigo
-    const nombre = texto.value.trim(); // Elimina espacios en blanco antes y después de la cadena de caracteres
-
-    // Validaciones
-    if (nombre === "") { // Cuando se haya introducido ningun caracter
+    const texto = document.getElementById("amigo"); 
+    const nombre = texto.value.trim(); 
+   
+    if (nombre === "") { 
         alert("Por favor, ingrese un nombre válido.");
         return;
     }
-    if (amigos.includes(nombre)) { // Cuando el nombre ya se introdujo en el arreglo
+    if (amigos.includes(nombre)) { 
         alert("Este nombre ya ha sido agregado.");
         return;
     }
 
-    amigos.push(nombre); // Agrega el nombre a la lista
+    amigos.push(nombre);
     actualizarLista();
-    texto.value = ""; // Limpia el texto almacenado
+    texto.value = ""; 
 }
 
-// Función para actualizar la lista de manera visual
+
 function actualizarLista() {
     const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // Limpiar la lista antes de actualizar
+    lista.innerHTML = ""; 
 
     for (let i = 0; i < amigos.length; i++) {
-        const li = document.createElement("li"); // Crear un nuevo <li>
-        li.textContent = amigos[i]; // Asignar el nombre del amigo
-        lista.appendChild(li); // Agregar el <li> a la lista
+        const li = document.createElement("li"); 
+        li.textContent = amigos[i]; 
+        lista.appendChild(li); 
     }
 }
 
-// Función para sortear un amigo secreto
+
 function sortearAmigo() {
     if (amigos.length < 3) {
         alert("Debe haber al menos 3 nombres para realizar el sorteo.");
         return;
     }
 
-    const indiceAleatorio = Math.floor(Math.random() * amigos.length); // Operacion para escoger un numero al azar segun la cantidad del arreglo
-    const amigoSeleccionado = amigos[indiceAleatorio]; // se guarda en la variable el nombre de la posicion del arreglo
-
-    const resultado = document.getElementById("resultado"); // Obtiene el elemento con id resultado
-    resultado.innerHTML = `<li> El amigo secreto es: ${amigoSeleccionado}</li>`; // Muestra el resultado
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length); 
+    const amigoSeleccionado = amigos[indiceAleatorio]; 
+    const resultado = document.getElementById("resultado"); 
+    resultado.innerHTML = `<li> El amigo secreto es: ${amigoSeleccionado}</li>`; 
 }
 
 
